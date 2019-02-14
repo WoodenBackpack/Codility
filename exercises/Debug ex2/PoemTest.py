d2616e9b03df46909104fd7e5ee9979ecec0bfc6
Pawel Morawski <morawski.pawel@outlook.com>
2019-02-14 14:05:30 +0100

    initial debug ex2 commit

--- exercises/Debug ex2/PoemTest.py
+++ exercises/Debug ex2/PoemTest.py
@@ -3,9 +3,9 @@ from Poem import *
 
 class PoemTest(unittest.TestCase):
     def setUp(self):
-        self.poem = Poem()
-    def testShouldCostTam(self):
-        self.assertEqual(1, 1)
+        pass
+    def testShouldReturnEmptyListWhenFileIsEmpty(self):
+        pass
 
 if __name__ == '__main__':
     unittest.main()
