fe506d0c29dbc6ba210a9952c674fdccbdd9be0a
Marcin Kacperski <marcin.kacperski@ericsson.com>
2019-02-20 08:40:56 +0100

    Unit tests

--- kasjer/ex_2/PoemTest.py
+++ kasjer/ex_2/PoemTest.py
@@ -3,13 +3,21 @@ from Poem import *
 
 class TestPoemTest(unittest.TestCase):
     def setUp(self):
-	print("dupa")
+        pass
 
-    def test_testShouldReturnEmptyListWhenFileIsEmpty(self):
-	listOfLines = getListOfNonEmptyLines("allEmptyLines.txt")
-	self.assertEqual(0, len(listOfLines), "List should be empty")
-	unittest.assertEqual(0, len(listOfLines), "List should be empty")
+    def test_ShouldReturnEmptyListWhenFileIsEmpty(self):
+        listOfLines = getListOfNonEmptyLines("allEmptyLines.txt")
+        self.assertEqual(0, len(listOfLines), "List should be empty")
+
+    def test_ShouldReturnNumberOfNonEmptyLinesFromFileWithoutEmptyLines(self):
+        listOfLines = getListOfNonEmptyLines("fiveNonEmptyLines.txt")
+        self.assertEqual(5, len(listOfLines), "List should not be empty")
+
+    def test_ShouldReturnNumberOfNonEmptyLinesFromFileWithEmptyLines(self):
+        listOfLines = getListOfNonEmptyLines("fiveLinesTwoEmpty.txt")
+        self.assertEqual(3, len(listOfLines), "List should not be empty")
+
 
 if __name__ == '__main__':
     unittest.main()
-    print("dupa")
+
